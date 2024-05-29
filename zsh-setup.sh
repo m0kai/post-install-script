afed460abd4d6c947f7272caa134994b95657609
m0kai <m0kai@proton.me>
2024-05-29 16:09:31 -0500

    removed extra script and added to package install.

--- zsh-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo pacman -S zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-wget -O ~/.oh-my-zsh/themes/heapbytes.zsh-theme https://raw.githubusercontent.com/m0kai/arch-dotfiles/main/zsh/heapbytes.zsh-theme
-wget -O ~/.zshrc https://raw.githubusercontent.com/m0kai/arch-dotfiles/main/zsh/.zshrc
