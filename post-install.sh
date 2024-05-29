3e7c8c6b21f23063942426a9693a02135d1bb21a
m0kai <m0kai@proton.me>
2024-05-29 09:45:23 -0500

    added print statements to post install script to show me what step of the process the script is on as its running. I also added a separate script to setup zsh to my theme and what not, may be able to consolodate later but for testing I want to try setting that up in a script first.

--- post-install.sh
+++ post-install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Setup Blackarch repository for pacman
+# Setup Blackarch repository for pacmani
+echo " ---- Setting up BlackArch ---- "
 curl -O https://blackarch.org/strap.sh
 chmod +x strap.sh
 ./strap.sh
 sudo pacman -Syu # remember to add sudo
 
+echo " ---- Installing Packages ---"
 # installing hardware specific packages for GPU stuff
 sudo pacman -S intel-compute-runtime --noconfirm
 
@@ -13,6 +15,7 @@ sudo pacman -S intel-compute-runtime --noconfirm
 # remember to add sudo
 sudo pacman -S nmap firefox neovim base-devel git intel-ucode gnu-netcat metasploit exploitdb ffuf burpsuite code thunar obsidian zsh net-tools ttf-font-awesome fastfetch rofi brightnessctl pamixer hashcat seclists --noconfirm
 
+echo " ---- Installing yay ---- "
 # Download/Install yay for AUR
 mkdir -p Software
 cd Software
@@ -23,6 +26,7 @@ cd ~
 
 yay ticktick notion-app
 
+echo " ---- Setting up OS Config ---- "
 # make sure config directories exist
 mkdir -p ~/.config/hypr
 mkdir -p ~/.config/kitty
@@ -36,8 +40,22 @@ wget -O ~/.config/kitty/kitty.conf https://raw.githubusercontent.com/m0kai/arch-
 wget -O ~/.config/waybar/config https://raw.githubusercontent.com/m0kai/arch-dotfiles/main/waybar/config
 wget -O ~/.config/waybar/style.css https://raw.githubusercontent.com/m0kai/arch-dotfiles/main/waybar/style.css
 
+echo " ---- Setting up Wordlists ---- "
 # setup and download wordlists
 sudo mkdir -p /usr/share/wordlists
+sudo mkdir -p /usr/share/wordlists/dirb
+sudo mkdir -p /usr/share/wordlists/dirbuster
 sudo mv /usr/share/seclists /usr/share/wordlists
 sudo wget -O /usr/share/wordlists/rockyou.txt.gz https://github.com/praetorian-inc/Hob0Rules/raw/master/wordlists/rockyou.txt.gz
 sudo gunzip /usr/share/wordlists/rockyou.txt.gz
+sudo wget -O /usr/share/wordlists/dirb/common.txt https://raw.githubusercontent.com/v0re/dirb/master/wordlists/common.txt
+sudo wget -O /usr/share/wordlists/dirb/big.txt https://raw.githubusercontent.com/v0re/dirb/master/wordlists/big.txt
+sudo wget -O /usr/share/wordlists/dirbuster/directory-list-2.3-big.txt https://raw.githubusercontent.com/daviddias/node-dirbuster/master/lists/directory-list-2.3-big.txt
+
+echo " ---- Setting up SSH Key for Github ---- "
+# Setup SSH key for GitHub
+ssh-keygen -t ed25519 -C "m0kai@github.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+echo "Copy+Paste the following Public Key to GitHub:"
+cat ~/.ssh/id_ed25519.pub
