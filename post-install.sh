76942e35ff70eff78b5e7cd759df9393541e3aa4
m0kai <m0kai@proton.me>
2024-06-06 12:27:22 -0500

    added install for zsh auto-complete and syntax highlighting for my zsh shell.

--- post-install.sh
+++ post-install.sh
@@ -80,6 +80,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 # Download theme and my configured .zshrc file
 wget -O ~/.oh-my-zsh/themes/heapbytes.zsh-theme https://raw.githubusercontent.com/m0kai/arch-dotfiles/main/zsh/heapbytes.zsh-theme
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 wget -O ~/.zshrc https://raw.githubusercontent.com/m0kai/arch-dotfiles/main/zsh/.zshrc
 
 echo "Copy+Paste the following Public Key to GitHub:"
