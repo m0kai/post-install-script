30e29cb26fde3656298eba60bd7b312237a5f053
m0kai <m0kai@proton.me>
2024-06-28 07:49:45 -0500

    took out some package installs, added git install earlier, and added LazyVim setup.

--- install.sh
+++ install.sh
@@ -6,29 +6,28 @@ NOCOLOR='\033[0m'
 GREEN='\033[0;32m'
 
 # Array Definitions
-PACMANPACKAGES=("nmap" "neovim" "base-devel" "git" "intel-ucode" "gnu-netcat" "metasploit" "exploitdb" "ffuf" "burpsuite" "code" "thunar" "obsidian" "zsh" "net-tools" "ttf-font-awesome" "fastfetch" "brightnessctl" "pamixer" "seclists" "waybar" "openvpn" "hydra" "discord" "vivaldi" "bluez" "bluez-utils" "blueberry" "mousepad" "signal-desktop" "gnome-themes-extra" "steam")
-YAYPACKAGES=("ticktick" "notion-app" "xmind" "1password" "nwg-drawer" "opera")
+PACMANPACKAGES=("nmap" "neovim" "base-devel" "intel-ucode" "gnu-netcat" "metasploit" "exploitdb" "ffuf" "burpsuite" "code" "thunar" "obsidian" "zsh" "net-tools" "ttf-font-awesome" "ttf-bigblueterminal-nerd" "fastfetch" "brightnessctl" "pamixer" "seclists" "waybar" "openvpn" "hydra" "discord" "vivaldi" "bluez" "bluez-utils" "blueberry" "mousepad" "signal-desktop" "gnome-themes-extra" "steam")
+YAYPACKAGES=("ticktick" "notion-app" "1password" "nwg-drawer")
 
 # function definitions
 validate_operation() {
 	local exit_code=$1
 	local message=$2
 
-
-	if [ $exit_code == 0 ];then
+	if [ $exit_code == 0 ]; then
 		printf "${message} ${GREEN}successful ${NOCOLOR}\n"
 	else
 		printf "${message} ${RED}failed ${NOCOLOR}\n"
 	fi
 }
 
-nvidia_driver_install () {
-	echo 'QT_QPA_PLATFORMTHEME="wayland;xcb"' | sudo tee -a /etc/environment > /dev/null
-	echo 'GBM_BACKEND=nvidia-drm' | sudo tee -a /etc/environment > /dev/null
-	echo '__GLX_VENDOR_LIBRARY_NAME=nvidia' | sudo tee -a /etc/environment > /dev/null
-	echo 'ENABLE_VKBASALT=1' | sudo tee -a /etc/environment > /dev/null
-	echo 'LIBVA_DRIVER_NAME=nvidia' | sudo tee -a /etc/environment > /dev/null
-	echo 'WLR_NO_HARDWARE_CURSORS=1' | sudo tee -a /etc/environment > /dev/null
+nvidia_driver_install() {
+	echo 'QT_QPA_PLATFORMTHEME="wayland;xcb"' | sudo tee -a /etc/environment >/dev/null
+	echo 'GBM_BACKEND=nvidia-drm' | sudo tee -a /etc/environment >/dev/null
+	echo '__GLX_VENDOR_LIBRARY_NAME=nvidia' | sudo tee -a /etc/environment >/dev/null
+	echo 'ENABLE_VKBASALT=1' | sudo tee -a /etc/environment >/dev/null
+	echo 'LIBVA_DRIVER_NAME=nvidia' | sudo tee -a /etc/environment >/dev/null
+	echo 'WLR_NO_HARDWARE_CURSORS=1' | sudo tee -a /etc/environment >/dev/null
 	echo 'Put the following into MODULES=() in  /etc/mkinitcpio.conf'
 	echo 'nvidia nvidia_modeset nvidia_uvm nvidia_drm'
 	read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
@@ -39,25 +38,27 @@ nvidia_driver_install () {
 }
 
 # keeping as an example for now
-#printf "standard ${RED}red ${NOCOLOR}no color ${GREEN}green\n" 
+#printf "standard ${RED}red ${NOCOLOR}no color ${GREEN}green\n"
 # Add to end of command to suppress output: >/dev/null 2>&1
 
 # Actual start of script
 printf "Setup nvidia proprietary driver config? (y/n) \n"
 read answer
 
-if [ $answer == "y" ];then
+if [ $answer == "y" ]; then
 	nvidia_driver_install
 fi
 
 printf " ---- Setting up SSH Key for Github ---- \n"
 # Setup SSH key for GitHub
 ssh-keygen -t ed25519 -C "m0kai@github.com"
-eval "$(ssh-agent -s)" 
+eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 echo "Copy+Paste the following Public Key to GitHub:"
 cat ~/.ssh/id_ed25519.pub
 read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+sudo pacman -S git --noconfirm
 
 printf " ---- Installing yay --- \n"
 mkdir -p Software
@@ -76,11 +77,9 @@ sudo make install
 cd ~
 
 printf " ---- Installing AUR Software ---- \n"
-for package in ${YAYPACKAGES[@]};
-do
-	yay -S $package 
+for package in ${YAYPACKAGES[@]}; do
+	yay -S $package
 done
-
 
 printf " ---- Setting up BlackArch ---- \n"
 curl -O https://blackarch.org/strap.sh >/dev/null 2>&1
@@ -92,7 +91,7 @@ sudo ./strap.sh >/dev/null 2>&1
 CODE=$?
 validate_operation $CODE "Black Arch install:"
 
-sudo wget -O /etc/pacman.conf  https://raw.githubusercontent.com/m0kai/arch-dotfiles/main/pacman/pacman.conf >/dev/null 2>&1
+sudo wget -O /etc/pacman.conf https://raw.githubusercontent.com/m0kai/arch-dotfiles/main/pacman/pacman.conf >/dev/null 2>&1
 CODE=$?
 validate_operation $CODE "pacman config download:"
 
@@ -102,8 +101,7 @@ CODE=$?
 validate_operation $CODE "Pacman Update:"
 
 printf " ---- Installing $(GREEN)Pacman $(NOCOLOR) Packages ---- \n"
-for package in ${PACMANPACKAGES[@]};
-do
+for package in ${PACMANPACKAGES[@]}; do
 	sudo pacman -S $package --noconfirm >/dev/null 2>&1
 	CODE=$?
 	validate_operation $CODE "${package} install:"
@@ -225,12 +223,11 @@ validate_operation $CODE "zsh config install"
 # printf " ---- Setting up SSH Key for Github ---- \n"
 # # Setup SSH key for GitHub
 # ssh-keygen -t ed25519 -C "m0kai@github.com"
-# eval "$(ssh-agent -s)" 
+# eval "$(ssh-agent -s)"
 # ssh-add ~/.ssh/id_ed25519
 # echo "Copy+Paste the following Public Key to GitHub:"
 # cat ~/.ssh/id_ed25519.pub
 # read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-
 
 printf " ---- Pulling Down Repos ---- \n"
 mkdir -p ~/Repos
@@ -257,3 +254,13 @@ git config --global user.name "m0kai"
 cd ~
 mkdir -p ~/VPNs
 
+# Setup LazyVim
+pirntf " ---- Setting up LazyVim  ---- \n"
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+rm -rf ~/.local/state/nvim
+rm -rf ~/.cache/nvim
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+printf "Open nvim to finish setup!\n"
+printf "Goodbye!\n"
